e91d59aa3300408fa6a32412b279b8bbd3fd92cd
Isaac Lemus <ilemus@umich.edu>
2016-05-11 20:09:55 -0400

    First Seven Functions created

--- main.lisp
+++ main.lisp
@@ -32,27 +32,67 @@
     )
 )
 
+;A function CHOICES which takes the name of the room and
+;   returns the table of permissible choices for Robbie's next
+;   destination.
 (defun choices (x)
     (searchfor rooms x)
 )
 
+;An expression which sets a global variable LOC to hold
+;   Robbie's current position in the PANTRY.
 (defun setLocation (x)
-    (setq loc x)
+    (setf loc x)
+)
+
+(setlocation 'pantry)
+
+;A predicate function UPSTAIRSP that returns T if it’s input
+;   is an upstairs locations
+(defun upstairsp (mLoc)
+    (cond 
+        ((equal 'UPSTAIRS-BEDROOM mLoc))
+        ((equal 'LIBRARY mLoc))
+    )
+)
+
+;A predicate function ONSTAIRSP which returns T if it’s input
+;   is either FRONT-STAIRS or BACK-STAIRS.
+(defun onstairsp (mLoc)
+    (cond 
+        ((equal 'FRONT-STAIRS mLoc))
+        ((equal 'BACK-STAIRS mLoc))
+    )
+)
+
+;A function WHERE that requires no inputs and tells Robbie
+;   where he is.
+(defun where ()
+    (cond 
+        ((upstairsp loc) (append (list 'ROBBIE 'IS 'UPSTAIRS 'IN 'THE) (list loc)))
+        ((onstairsp loc)  (append (list 'ROBBIE 'IS 'ON 'THE) (list loc)))
+        (t (append (list 'ROBBIE 'IS 'DOWNSTAIRS 'IN 'THE) (list loc)))
+    )
 )
 
 (defun count-for (mlist mcount)
     (if (equal nil mlist)
         mcount
-        (count-for (cdr mlist) mcount)
+        (count-for (cdr mlist) (1+ mcount))
     )
 )
 
+;A function HOW-MANY-CHOICES that tells how many choices
+;   Robbie has for where he may move given the current value of
+;   the variable LOC.
 (defun how-many-choices ()
-    (setq mtemp (choices(loc)))
-    (setq mcount 0)
-    
+    (setq mtemp loc)
+    (count-for (choices mtemp) 0)
 )
 
+;The function LOOK that takes two inputs, a direction and a
+;   room, and tells Robbie where he will end up if he moved in
+;   that direction from that room.
 (defun look (dir loc)
     (setq mtemp (choices loc))
     (if (listp mtemp)
